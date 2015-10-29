fe953e809cdf7ae4d8b58706874d05c319cc3667
Ulf Ekstrom <uekstrom@gmail.com>
2015-10-29 16:13:58 +0100

    Update the MIT license.

--- polymul.hpp
+++ polymul.hpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2009 Ulf Ekstrom <uekstrom@gmail.com>
+Copyright (c) 2009-2015 Ulf Ekstrom <uekstrom@gmail.com>
 
 Permission is hereby granted, free of charge, to any person
 obtaining a copy of this software and associated documentation
