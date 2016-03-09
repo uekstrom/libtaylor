e401662b71bd52432bc8e7ea2801a3270f7bb250
Ulf Ekstrom <uekstrom@gmail.com>
2016-03-09 16:06:11 +0100

    Composition.

--- polyops.cpp
+++ polyops.cpp
@@ -1,128 +1,45 @@
-#ifndef MIN
-#define MIN(x,y) ((x) < (y) ? (x) : (y))
-#endif
-#ifndef MAX
-#define MAX(x,y) ((x) > (y) ? (x) : (y))
-#endif
+#include<cstdio>
+#include "polyops.hpp"
 
-/* nchoosek(nvar+ndeg,ndeg) */
-int polylen(int nvar, int ndeg)
-{
-  int i, val = 1;
-  if (ndeg < 0) // this simplifies some recursions
-    return 0;
-  for (i=1;i<=ndeg;i++)
-    val = (val*(nvar+i))/i;
-  return val;    
-}
+#include "polymul.hpp"
 
-template<typename T>
-void polymul_add(int nvar,
-		 T c[],
-		 const T a[], int adeg,
-		 const T b[], int bdeg)
-{
-  int i,j;
-  if (nvar == 0)
-    {
-      c[0] += a[0]*b[0];
-    }
-  else
-    {
-      for (i=adeg+bdeg;i>=0;i--)
-	{
-	  int u = MIN(bdeg,i);
-	  int l = MAX(0,i-adeg);
-	  for (j=l;j<=u;j++)
-	    polymul_add(nvar-1, c + polylen(nvar,i-1), a + polylen(nvar,i-j-1), i-j, b + polylen(nvar,j-1), j);
-	}
-    }
-}
-
-template<typename T>
-void polymul_set(int nvar,
-		   T c[],
-		   const T a[], int adeg,
-		   const T b[], int bdeg)
-{
-  int i,j;
-  if (nvar == 0)
-    {
-      c[0] = a[0]*b[0];
-    }
-  else
-    {
-      for (i=adeg+bdeg;i>=0;i--)
-	{
-	  int u = MIN(bdeg,i);
-	  int l = MAX(0,i-adeg);
-	  polymul_set(nvar-1, c + polylen(nvar,i-1), a + polylen(nvar,i-l-1), i-l, b + polylen(nvar,l-1), l);
-	  for (j=l+1;j<=u;j++)
-	    polymul_add(nvar-1, c + polylen(nvar,i-1), a + polylen(nvar,i-j-1), i-j, b + polylen(nvar,j-1), j);
-	}
-    }
-}
-
-
-/* adeg = bdeg = ndeg, and truncate output at ndeg. */
-template<typename T>
-void taylormul_add(int nvar, int ndeg,
-		   T c[],
-		   const T a[],
-		   const T b[])
-{
-  int i,j;
-  if (nvar == 0)
-    {
-      c[0] += a[0]*b[0];
-    }
-  else
-    {
-      for (i=ndeg;i>=0;i--)
-	{
-	  for (j=0;j<=i;j++)
-	    polymul_add(nvar-1, c + polylen(nvar,i-1), a + polylen(nvar,i-j-1), i-j, b + polylen(nvar,j-1), j);
-	}
-    }
-}
-
-template<typename T>
-void taylormul_set(int nvar, int ndeg,
-		   T c[],
-		   const T a[],
-		   const T b[])
-{
-  int i,j;
-  if (nvar == 0)
-    {
-      c[0] = a[0]*b[0];
-    }
-  else
-    {
-      for (i=ndeg;i>=0;i--)
-	{
-	  polymul_set(nvar-1, c + polylen(nvar,i-1), a + polylen(nvar,i-1), i, b, 0);
-	  for (j=1;j<=i;j++)
-	    polymul_add(nvar-1, c + polylen(nvar,i-1), a + polylen(nvar,i-j-1), i-j, b + polylen(nvar,j-1), j);
-	}
-    }
-}
-
-#include <stdio.h>
-
-namespace polymul{
-#include "polymul.h"
-};
 
 int main()
 {
-  polymul::polynomial<double,4,3> a,b,ct;
-  polymul::polynomial<double,4,6> c,c2,aa;
+  polymul::polynomial<double,2,2> a,aa,b;
+  polymul::polynomial<double,1,2> c;
   int i;
-  for (int i=0;i<polylen(4,6);i++)
+  for (int i=0;i<polylen(1,2);i++)
     {
-      c[i] = 0;
-      c2[i] = 0;
+      c[i] = 1+i;
+    }
+  for (int i=0;i<polylen(2,2);i++)
+    {
+      a[i] = 0;
+      b[i] = 2+i;
+    }
+  b[0] = 0;
+  polymul::taylorcompose0<double,2,2,2>(a,b,c.c);
+  taylor_compose(2,2,aa.c,b.c,c.c);
+  //  taylormul_set(4,3,c.c,a.c,b.c);
+  for (i=0;i<polylen(2,2);i++)
+    {
+      if (a[i] != aa[i])
+	printf("%i %lf\n",i, a[i]-aa[i]);
+    }
+  return 0;
+} 
+
+
+int mainapa()
+{
+  polymul::polynomial<double,4,3> a,b,ct;
+  polymul::polynomial<double,4,3> c,c2,aa;
+  int i;
+  for (int i=0;i<polylen(4,3);i++)
+    {
+      c[i] = i;
+      c2[i] = i;
       aa[i] = 0;
     }
   for (int i=0;i<polylen(4,3);i++)
@@ -131,13 +48,16 @@ int main()
       a[i] = 1+i;
       b[i] = 2+i;
     }
-  polymul::polymul(c2,a,b);
-  polymul::taylormul(ct,a,b);
-  taylormul_set(4,3,c.c,a.c,b.c);
+  //polymul_add(4,c.c,a.c,3,b.c,3);
+  a[0] = 0;
+  polymul_add_trunc_noconstb(4,c.c,3,c.c,3,a.c,3);
+  polymul::taylormul(c2,a); // c2 = c2*a
+  
   for (i=0;i<polylen(4,3);i++)
     {
-      if (c[i] != ct[i])
-	printf("%i %lf\n",i, c[i]-ct[i]);
+      if (c[i] != c2[i]-i)
+	printf("%i %lf\n",i, c[i]-c2[i]-i);
     }
   return 0;
 }
+
---
+++ polyops.hpp
@@ -0,0 +1,223 @@
+#ifndef POLYOPS_HPP
+#define POLYOPS_HPP
+#include <cassert>
+
+#ifndef MIN
+#define MIN(x,y) ((x) < (y) ? (x) : (y))
+#endif
+#ifndef MAX
+#define MAX(x,y) ((x) > (y) ? (x) : (y))
+#endif
+
+/* nchoosek(nvar+ndeg,ndeg) */
+int polylen(int nvar, int ndeg)
+{
+  int i, val = 1;
+  if (ndeg < 0) // this simplifies some recursions
+    return 0;
+  for (i=1;i<=ndeg;i++)
+    val = (val*(nvar+i))/i;
+  return val;    
+}
+
+// For all of the functions below the c[] and a[] array may alias
+// and the outcome is still as expected.
+
+template<typename T>
+void polymul_add(int nvar,
+		 T c[],
+		 const T a[], int adeg,
+		 const T b[], int bdeg)
+{
+  int i,j;
+  if (nvar == 0)
+    {
+      c[0] += a[0]*b[0];
+    }
+  else
+    {
+      for (i=adeg+bdeg;i>=0;i--)
+	{
+	  int u = MIN(bdeg,i);
+	  int l = MAX(0,i-adeg);
+	  for (j=l;j<=u;j++)
+	    polymul_add(nvar-1, c + polylen(nvar,i-1), a + polylen(nvar,i-j-1), i-j, b + polylen(nvar,j-1), j);
+	}
+    }
+}
+
+// Output is truncated at order cdeg
+template<typename T>
+void polymul_add_trunc(int nvar,
+		       T c[], int cdeg,
+		 const T a[], int adeg,
+		 const T b[], int bdeg)
+{
+  int i,j;
+  assert(cdeg <= adeg+bdeg);
+  if (nvar == 0)
+    {
+      c[0] += a[0]*b[0];
+    }
+  else
+    {
+      for (i=cdeg;i>=0;i--)
+	{
+	  int u = MIN(bdeg,i);
+	  int l = MAX(0,i-adeg);
+	  for (j=l;j<=u;j++)
+	    polymul_add(nvar-1, c + polylen(nvar,i-1), a + polylen(nvar,i-j-1), i-j, b + polylen(nvar,j-1), j);
+	}
+    }
+}
+
+template<typename T>
+void polymul_set(int nvar,
+		   T c[],
+		   const T a[], int adeg,
+		   const T b[], int bdeg)
+{
+  int i,j;
+  if (nvar == 0)
+    {
+      c[0] = a[0]*b[0];
+    }
+  else
+    {
+      for (i=adeg+bdeg;i>=0;i--)
+	{
+	  int u = MIN(bdeg,i);
+	  int l = MAX(0,i-adeg);
+	  polymul_set(nvar-1, c + polylen(nvar,i-1), a + polylen(nvar,i-l-1), i-l, b + polylen(nvar,l-1), l);
+	  for (j=l+1;j<=u;j++)
+	    polymul_add(nvar-1, c + polylen(nvar,i-1), a + polylen(nvar,i-j-1), i-j, b + polylen(nvar,j-1), j);
+	}
+    }
+}
+
+
+/* adeg = bdeg = ndeg, and truncate output at ndeg. */
+template<typename T>
+void taylormul_add(int nvar, int ndeg,
+		   T c[],
+		   const T a[],
+		   const T b[])
+{
+  int i,j;
+  if (nvar == 0)
+    {
+      c[0] += a[0]*b[0];
+    }
+  else
+    {
+      for (i=ndeg;i>=0;i--)
+	{
+	  for (j=0;j<=i;j++)
+	    polymul_add(nvar-1, c + polylen(nvar,i-1), a + polylen(nvar,i-j-1), i-j, b + polylen(nvar,j-1), j);
+	}
+    }
+}
+
+template<typename T>
+void taylormul_set(int nvar, int ndeg,
+		   T c[],
+		   const T a[],
+		   const T b[])
+{
+  int i,j;
+  if (nvar == 0)
+    {
+      c[0] = a[0]*b[0];
+    }
+  else
+    {
+      for (i=ndeg;i>=0;i--)
+	{
+	  polymul_set(nvar-1, c + polylen(nvar,i-1), a + polylen(nvar,i-1), i, b, 0);
+	  for (j=1;j<=i;j++)
+	    polymul_add(nvar-1, c + polylen(nvar,i-1), a + polylen(nvar,i-j-1), i-j, b + polylen(nvar,j-1), j);
+	}
+    }
+}
+
+
+
+// c += a*b with b[0] implicitly set to 0.
+template<typename T>
+void polymul_add_trunc_noconstb(int nvar,
+				T c[], int cdeg,
+				const T a[], int adeg,
+				const T b[], int bdeg)
+{
+  int i,j;
+  assert(cdeg <= adeg+bdeg);
+  if (nvar == 0)
+    {
+      return;
+    }
+  else
+    {
+      for (i=cdeg;i>=0;i--)
+	{
+	  int u = MIN(bdeg,i);
+	  int l = MAX(0,i-adeg);
+	  if (l == 0)
+	    polymul_add_trunc_noconstb(nvar-1, c + polylen(nvar,i-1), i, a + polylen(nvar,i-1), i, b, 0);
+	  else
+	    polymul_add(nvar-1, c + polylen(nvar,i-1), a + polylen(nvar,i-l-1), i-l, b + polylen(nvar,l-1), l);
+	  for (j=l+1;j<=u;j++)
+	    polymul_add(nvar-1, c + polylen(nvar,i-1), a + polylen(nvar,i-j-1), i-j, b + polylen(nvar,j-1), j);
+	}
+    }
+}
+
+// c = a*b with b[0] implicitly set to 0.
+template<typename T>
+void polymul_set_trunc_noconstb(int nvar,
+				T c[], int cdeg,
+				const T a[], int adeg,
+				const T b[], int bdeg)
+{
+  int i,j;
+  assert(cdeg <= adeg+bdeg);
+  if (nvar == 0)
+    {
+      c[0] = 0;
+    }
+  else
+    {
+      for (i=cdeg;i>=0;i--)
+	{
+	  int u = MIN(bdeg,i);
+	  int l = MAX(0,i-adeg);
+	  if (l == 0)
+	    polymul_set_trunc_noconstb(nvar-1, c + polylen(nvar,i-1), i, a + polylen(nvar,i-1), i, b, 0);
+	  else
+	    polymul_set(nvar-1, c + polylen(nvar,i-1), a + polylen(nvar,i-l-1), i-l, b + polylen(nvar,l-1), l);
+	  for (j=l+1;j<=u;j++)
+	    polymul_add(nvar-1, c + polylen(nvar,i-1), a + polylen(nvar,i-j-1), i-j, b + polylen(nvar,j-1), j);
+	}
+    }
+}
+
+/*
+  Compute y = sum_i=0,ndeg c[i]*(x-x0)^i = c[0] + (x-x0)*(c[1] + (x-x0)*(..))
+ */
+template<typename T, typename S>
+void taylor_compose(int nvar, int ndeg,
+		   T y[], const T x[], const S c[])
+{
+  int plen = polylen(nvar,ndeg);
+  y[0] = c[ndeg];
+  for (int i=1;i<plen;i++)
+    y[i] = 0;
+  for (int i=ndeg-1;i>=0;i--)
+    {
+      polymul_set_trunc_noconstb(nvar,y,ndeg,y,ndeg,x,ndeg);
+      //      taylormul_add(nvar,ndeg,y,y,x);
+      y[0] += c[i];
+    }
+}
+
+
+#endif
